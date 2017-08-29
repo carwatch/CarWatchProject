65db8a84aa8f15bf9236de2756ff06c0c97b5347
carwatch <alonharbi21@gmail.com>
2017-08-29 23:14:28 +0300

    Added: GetContacts

--- CarWatch/Controllers/MessageController.cs
+++ CarWatch/Controllers/MessageController.cs
@@ -38,7 +38,7 @@ namespace CarWatch.Controllers
         }
 
         [BasicAuthentication]
-        [HttpGet]
+        [HttpPost]
         public async Task<IHttpActionResult> GetMessagesHistory([FromBody] MessageDetails i_Message)
         {
             string nickname = Thread.CurrentPrincipal.Identity.Name;
@@ -54,5 +54,43 @@ namespace CarWatch.Controllers
             }
         }
 
+        [BasicAuthentication]
+        [HttpPost]
+        public async Task<IHttpActionResult> GetContacts([FromBody] MessageDetails i_Message)
+        {
+            string nickname = Thread.CurrentPrincipal.Identity.Name;
+            if (i_Message.SenderNickname != nickname)
+            {
+                return BadRequest("Emails do not match.");
+            }
+
+            using (CarWatchDBEntities entities = new CarWatchDBEntities())
+            {
+                List<string> nicknameList = new List<string>();
+                var messages = await entities.Messages.Where(e => (e.SenderNickname == i_Message.SenderNickname) || (e.ReceiverNickname == i_Message.SenderNickname)).ToListAsync();
+                foreach (Message msg in messages)
+                {
+                    if (i_Message.SenderNickname != msg.SenderNickname)
+                    {
+                        if (!nicknameList.Contains(msg.SenderNickname))
+                            nicknameList.Add(msg.SenderNickname);
+                    }
+                    else
+                    {
+                        if (!nicknameList.Contains(msg.ReceiverNickname))
+                            nicknameList.Add(msg.ReceiverNickname);
+                    }
+                }
+
+                FacebookAccount account;
+                List<FacebookAccount> fbList = new List<FacebookAccount>();
+                foreach (string item in nicknameList)
+                {
+                    account = await entities.FacebookAccounts.Where(e => e.Nickname == item).FirstOrDefaultAsync();
+                    fbList.Add(account);
+                }
+                return Ok(fbList);
+            }
+        }
     }
 }
